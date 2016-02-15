5fee7ec79534d7752eaf227420a6515e512d0949
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2016-02-15 00:02:28 +0000

    fmt

--- types.go
+++ types.go
@@ -27,105 +27,105 @@ type Quote struct {
 }
 
 type TickerQuote struct {
-    Ok                  bool       `json:"ok"`
-    Error               string     `json:"error,omitempty"`
-    Quote               Quote      `json:"quote"`
+    Ok                  bool        `json:"ok"`
+    Error               string      `json:"error,omitempty"`
+    Quote               Quote       `json:"quote"`
 }
 
 type Execution struct {
-    Ok                  bool       `json:"ok"`
-    Error               string     `json:"error,omitempty"`
-    Account             string     `json:"account"`
-    Venue               string     `json:"venue"`
-    Symbol              string     `json:"symbol"`
-    Order               Order      `json:"order"`
-    StandingId          int        `json:"standingId"`
-    IncomingId          int        `json:"incomingId"`
-    Price               int        `json:"price"`
-    Filled              int        `json:"filled"`
-    FilledAt            string     `json:"filledAt"`
-    StandingComplete    bool       `json:"standingComplete"`
-    IncomingComplete    bool       `json:"incomingComplete"`
+    Ok                  bool        `json:"ok"`
+    Error               string      `json:"error,omitempty"`
+    Account             string      `json:"account"`
+    Venue               string      `json:"venue"`
+    Symbol              string      `json:"symbol"`
+    Order               Order       `json:"order"`
+    StandingId          int         `json:"standingId"`
+    IncomingId          int         `json:"incomingId"`
+    Price               int         `json:"price"`
+    Filled              int         `json:"filled"`
+    FilledAt            string      `json:"filledAt"`
+    StandingComplete    bool        `json:"standingComplete"`
+    IncomingComplete    bool        `json:"incomingComplete"`
 }
 
 type Heartbeat struct {
-    Ok                  bool       `json:"ok"`
-    Error               string     `json:"error"`
+    Ok                  bool        `json:"ok"`
+    Error               string      `json:"error"`
 }
 
 type VenueHeartbeat struct {
-    Ok                  bool       `json:"ok"`
-    Error               string     `json:"error,omitempty"`
-    Venue               string     `json:"venue"`
+    Ok                  bool        `json:"ok"`
+    Error               string      `json:"error,omitempty"`
+    Venue               string      `json:"venue"`
 }
 
 type Venue struct {
-    Id                  int        `json:"id"`
-    Name                string     `json:"name"`
-    Venue               string     `json:"venue"`
-    State               string     `json:"state"`
+    Id                  int         `json:"id"`
+    Name                string      `json:"name"`
+    Venue               string      `json:"venue"`
+    State               string      `json:"state"`
 }
 
 type VenueList struct {
-    Ok                  bool       `json:"id"`                 // Bug on official server
-    Error               string     `json:"error,omitempty"`
+    Ok                  bool        `json:"id"`                 // Bug on official server
+    Error               string      `json:"error,omitempty"`
     Venues              []Venue     `json:"venues"`
 }
 
 type Symbol struct {
-    Name                string     `json:"name"`
-    Symbol              string     `json:"symbol"`
+    Name                string      `json:"name"`
+    Symbol              string      `json:"symbol"`
 }
 
 type StockList struct {
-    Ok                  bool       `json:"ok"`
-    Error               string     `json:"error,omitempty"`
+    Ok                  bool        `json:"ok"`
+    Error               string      `json:"error,omitempty"`
     Symbols             []Symbol    `json:"symbols"`
 }
 
 type BookEntry struct {
-    Price               int        `json:"price"`
-    Qty                 int        `json:"qty"`
-    IsBuy               bool       `json:"isBuy"`
+    Price               int         `json:"price"`
+    Qty                 int         `json:"qty"`
+    IsBuy               bool        `json:"isBuy"`
 }
 
 type OrderBook struct {
-    Ok                  bool       `json:"ok"`
-    Error               string     `json:"error,omitempty"`
-    Venue               string     `json:"venue"`
-    Symbol              string     `json:"symbol"`
-    Ts                  string     `json:"ts"`
+    Ok                  bool        `json:"ok"`
+    Error               string      `json:"error,omitempty"`
+    Venue               string      `json:"venue"`
+    Symbol              string      `json:"symbol"`
+    Ts                  string      `json:"ts"`
     Bids                []BookEntry `json:"bids"`
     Asks                []BookEntry `json:"asks"`
 }
 
 type Fill struct {
-    Price               int        `json:"price"`
-    Qty                 int        `json:"qty"`
-    Ts                  string     `json:"ts"`
+    Price               int         `json:"price"`
+    Qty                 int         `json:"qty"`
+    Ts                  string      `json:"ts"`
 }
 
 type Order struct {
-    Ok                  bool       `json:"ok"`
-    Error               string     `json:"error,omitempty"`
-    Account             string     `json:"account"`
-    Venue               string     `json:"venue"`
-    Symbol              string     `json:"symbol"`
-    Direction           string     `json:"direction"`
-    OrderType           string     `json:"orderType"`
-    Ts                  string     `json:"ts"`
-    OriginalQty         int        `json:"originalQty"`
-    Qty                 int        `json:"qty"`
-    Price               int        `json:"price"`
-    TotalFilled         int        `json:"totalFilled"`
-    Id                  int        `json:"id"`
+    Ok                  bool        `json:"ok"`
+    Error               string      `json:"error,omitempty"`
+    Account             string      `json:"account"`
+    Venue               string      `json:"venue"`
+    Symbol              string      `json:"symbol"`
+    Direction           string      `json:"direction"`
+    OrderType           string      `json:"orderType"`
+    Ts                  string      `json:"ts"`
+    OriginalQty         int         `json:"originalQty"`
+    Qty                 int         `json:"qty"`
+    Price               int         `json:"price"`
+    TotalFilled         int         `json:"totalFilled"`
+    Id                  int         `json:"id"`
     Fills               []Fill      `json:"fills"`
-    Open                bool       `json:"open"`
+    Open                bool        `json:"open"`
 }
 
 type OrderList struct {
-    Ok                  bool       `json:"ok"`
-    Error               string     `json:"error,omitempty"`
+    Ok                  bool        `json:"ok"`
+    Error               string      `json:"error,omitempty"`
     Orders              []Order     `json:"orders"`
 }
 
