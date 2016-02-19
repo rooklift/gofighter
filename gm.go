6916ec9c4f042a2e52f736435e20f1264378c9b1
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2016-02-19 19:59:56 +0000

    more GM work

--- gm.go
+++ gm.go
@@ -1,6 +1,11 @@
 package gofighter
 
 import (
+    "bufio"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "os"
     "strconv"
 )
 
@@ -12,23 +17,135 @@ type Level struct {
     Venues []string         `json:"venues"`
     Tickers []string        `json:"tickers"`
     Balances map[string]int `json:"balances,omitempty"`
-    BaseURL string          `json:"baseURL,omitempty"`       // These ain't part of the official response
-    WebSocketURL string     `json:"websocketURL,omitempty"`  // but might be present in custom things...
+    BaseURL string          `json:"baseURL,omitempty"`       // These ain't part of the
+    WebSocketURL string     `json:"websocketURL,omitempty"`  // official response but might
+	ApiKey string           `json:"apiKey,omitempty"`        // be present in custom things
 }
 
+const OFFICIAL_BASE_URL = "https://api.stockfighter.io/ob/api"
+const OFFICIAL_WS_URL = "https://api.stockfighter.io/ob/api/ws"
 const OFFICIAL_GM_URL = "https://www.stockfighter.io/gm"
 
+const GM_DIRECTORY = "gm"
 
-func GMstart(info TradingInfo, levelname string)  (Level, error) {
+
+func GMstart(apikey string, levelname string)  (Level, error) {
+
+    var err error
+    if apikey == "" {
+        apikey, err = LoadAPIKey("api_key.txt")
+        if err != nil {
+            fmt.Println(err)
+        }
+    }
+
     url := OFFICIAL_GM_URL + "/levels/" + levelname
     var ret Level
-    err := get_json_from_url("POST", url, info.ApiKey, nil, &ret)
+    err = get_json_from_url("POST", url, apikey, nil, &ret)
     return ret, err
 }
 
-func GMstop(info TradingInfo, instance int)  (Heartbeat, error) {
+func GMstop(apikey string, instance int)  (Heartbeat, error) {
+
+    var err error
+    if apikey == "" {
+        apikey, err = LoadAPIKey("api_key.txt")
+        if err != nil {
+            fmt.Println(err)
+        }
+    }
+
     url := OFFICIAL_GM_URL + "/instances/" + strconv.Itoa(instance) + "/stop"
     var ret Heartbeat
-    err := get_json_from_url("POST", url, info.ApiKey, nil, &ret)
+    err = get_json_from_url("POST", url, apikey, nil, &ret)
     return ret, err
+}
+
+func LoadGMfile(levelname string)  (Level, error) {
+    filename := GM_DIRECTORY + "/" + levelname + "_info.txt"
+
+    var ret Level
+
+    s, err := ioutil.ReadFile(filename)
+	if err != nil {
+        ret.Error = err.Error()
+		return ret, err
+	}
+
+    err = json.Unmarshal(s, &ret)
+    if err != nil {
+        ret.Error = err.Error()
+        return ret, err
+    }
+
+	return ret, nil
+}
+
+func SaveGMfile(levelname string, level Level)  {
+    filename := GM_DIRECTORY + "/" + levelname + "_info.txt"
+    os.Mkdir(GM_DIRECTORY, 0777)
+
+    s, _ := MakeJSON(level)
+    err := ioutil.WriteFile(filename, []byte(s), 0777)
+    if err != nil {
+        fmt.Println(err)
+    }
+    return
+}
+
+func TradingInfoFromLevel(level Level)  TradingInfo {
+
+    info := TradingInfo{}
+
+    info.BaseURL = level.BaseURL
+    info.WebSocketURL = level.WebSocketURL
+    info.ApiKey = level.ApiKey
+    info.Account = level.Account
+    if len(level.Venues) > 0 {
+        info.Venue = level.Venues[0]
+    }
+    if len(level.Tickers) > 0 {
+        info.Symbol = level.Tickers[0]
+    }
+
+    if info.BaseURL == "" {info.BaseURL = OFFICIAL_BASE_URL}
+    if info.WebSocketURL == "" {info.WebSocketURL = OFFICIAL_WS_URL}
+    if info.ApiKey == "" {
+        var err error
+        info.ApiKey, err = LoadAPIKey("api_key.txt")
+        if err != nil {
+            fmt.Println(err)
+        }
+    }
+
+    return info
+}
+
+func TradingInfoFromName(levelname string)  TradingInfo {
+
+    // Assuming we have already contacted the GM and written a GM info file,
+    // this function returns a TradingInfo struct, using sensible default
+    // values for the URLs and API key.
+
+    level, _ := LoadGMfile(levelname)
+    return TradingInfoFromLevel(level)
+}
+
+func NameFromUser()  string {
+    var known = make(map[string]string)
+
+    s, err := ioutil.ReadFile("known_levels.json")
+	if err != nil {
+        fmt.Println(err)
+	}
+
+    json.Unmarshal(s, &known)
+    PrintJSON(known)
+
+    fmt.Println("\nEnter level number...")
+
+    scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+    return known[scanner.Text()]
 }
